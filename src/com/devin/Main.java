d44ac23ed321ded66029fc25f432c3ac30f40689
DevinKott-RIT <dak3386@g.rit.edu>
2017-10-05 12:14:51 -0400

    initial commit

---
+++ src/com/devin/Main.java
@@ -0,0 +1,60 @@
+package com.devin;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		final String INPUT_FILE = "3x3_test.txt";
+		float[][] matrix = readMatrixFromFile(INPUT_FILE);
+		print_matrix(matrix);
+	}
+	
+	private static void print_matrix(float[][] matrix) {
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		int cur_row = 0, cur_col = 0;
+		for (; cur_row < rows; cur_row++) {
+			for (cur_col = 0; cur_col < cols - 1; cur_col++) {
+				System.out.printf("%.2f ", matrix[cur_row][cur_col]);
+			}
+			System.out.printf("%.2f\n", matrix[cur_row][cur_col - 1]);
+		}
+	}
+
+	private static float[][] readMatrixFromFile(String inputFile) {
+		float[][] matrix = null;
+		try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+			String s = "";
+			s = br.readLine();
+			String[] parts = s.split("\\s+");
+			int num_rows = Integer.parseInt(parts[0]);
+			int num_cols = Integer.parseInt(parts[1]);
+			matrix = new float[num_rows][num_cols];
+			int cur_row = 0;
+			int cur_col = 0;
+			while ((s = br.readLine()) != null) {
+				parts = s.split("\\s+");
+				if (parts.length != num_cols) {
+					System.err.printf("incorrect number of elements in row %d\n", cur_row);
+					continue;
+				}
+				for (cur_col = 0; cur_col < num_cols; cur_col++) {
+					float f = Float.parseFloat(parts[cur_col]);
+					matrix[cur_row][cur_col] = f;
+				}
+				cur_row++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return matrix;
+	}
+
+}
